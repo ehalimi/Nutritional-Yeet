01ccba5c5ef5327e0667bbc8b80ca5e65baf51b7
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-11-02 16:37:54 -0500

    fixed more merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import {
@@ -82,7 +59,6 @@ function App() {
         </Routes>
       </Router>
     </ApolloProvider>
->>>>>>> bethany
   );
 }
 
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,16 +1,10 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-=======
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nutritionalyeet', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
->>>>>>> bethany
 });
 
 module.exports = mongoose.connection;
