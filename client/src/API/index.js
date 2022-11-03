dd7ebc0fe57adc20f2d1eba2feb6a5eb2e3bbe4d
Ema Halimi <ehalimi@na.edu>
2022-11-02 22:05:34 -0500

    debugging

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,13 +1,13 @@
-const url = "https://nutrition-api.esha.com/nutrients";
+// const url = "https://nutrition-api.esha.com/nutrients";
 
-const api = {
-    nutrients: function () {
-        return fetch(`${url}nutrients`, {
-            headers: {
-                "Ocp-Apim-Subscription-Key": "ec953442bf8249358f75864e6137a33c"
-            }
-        })
-    }
-};
+// const api = {
+//     nutrients: function () {
+//         return fetch(`${url}nutrients`, {
+//             headers: {
+//                 "Ocp-Apim-Subscription-Key": "ec953442bf8249358f75864e6137a33c"
+//             }
+//         })
+//     }
+// };
 
-module.exports = api;
+// module.exports = api;
